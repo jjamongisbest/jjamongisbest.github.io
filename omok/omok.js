6500f581d6cd923acd80aa95328f5c1155c7c24c
jjamongisbest <jieun98514@gmail.com>
2023-03-13 18:18:10 +0900

    Update Site

--- omok/omok.js
+++ omok/omok.js
@@ -1,11 +1,31 @@
 const gameContainer = document.getElementById("gameContainer");
+const grid = document.getElementById("grid");
 
 const map = document.createElement("div");
 map.className = "map";
 
+const setGrid = document.createElement("div");
+grid.id = "setGrid";
+
 let winner = 0;
 let turn = 1;
 const size = 10;
+
+setGrid();
+function setGrid() {
+  for (let i = 0; i < size; i++) {
+    for (let k = 0; k < size; k++) {
+      const box = document.createElement("div");
+
+      const id = `y${i}x${k}`;
+      box.id = id;
+      box.className = "tile";
+
+      setGrid.append(box);
+    }
+  }
+  grid.append(setGrid);
+}
 
 setMap();
 
