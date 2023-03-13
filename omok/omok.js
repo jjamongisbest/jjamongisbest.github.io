019b17c682ae782d6ec18ef8821683f4d4c91119
jjamongisbest <jieun98514@gmail.com>
2023-03-13 09:40:41 +0900

    Update Omok

--- omok/omok.js
+++ omok/omok.js
@@ -5,11 +5,17 @@ map.className = "map";
 
 let winner = 0;
 let turn = 1;
-let size = 10;
+const size = 10;
 
 setMap();
 
 function setMap(){
+    // const mapContainer = document.createElement("div");
+    // mapContainer.id = "mapContainer";
+
+    // const mapContainer2 = document.createElement("div");
+    // mapContainer.id = "mapContainer2";
+
     for(let i = 0; i< size; i++){
         for(let k = 0; k<size; k++){
             const tile = document.createElement("div");
@@ -24,13 +30,124 @@ function setMap(){
                 console.log(id);
 
                 const target = e.target;
-            })
+
+                if(target.innerText){
+                    alert("이미 선택 된 것이다!!!!!!!!!");
+                    return;
+                }
+
+                tile.innerText = turn == 1 ? "⚫️":"⚪️";
+
+                checkWinner();
+
+                if(winner !== 0){
+                    alert(`winner: ${winner}`);
+                }
+
+                turn = turn === 1 ? 2 : 1;
+            });
+
         }
     }
 
     gameContainer.append(map);
 }
 
-function fakeMap(){
-    
+
+function checkWinner() {
+    checkHori();
+    checkVerti();
+    checkDiag();
+    checkDiag2();
+}
+
+function checkHori(){
+    for(let i=0; i<size; i++){
+        let count = 0;
+        for(let j=0; j<size; j++){
+            const target = `y${i}x${j}`;
+            const box = map.querySelector(`#${target}`);
+            const text = box.innerText;
+            if(text === (turn == 1 ? "⚫️" : "⚪️")) {
+                count ++;
+            }
+
+            if(count === 5){
+                winner = turn;
+            }
+        }
+    }
+}
+
+function checkVerti(){
+    for(let i=0; i<size; i++){
+        let count = 0;
+        for(let j=0; j<size; j++){
+            const target = `y${j}x${i}`;
+            const box = map.querySelector(`#${target}`);
+            const text = box.innerText;
+            if(text === (turn == 1 ? "⚫️" : "⚪️")) {
+                console.log(count);
+                count ++;
+            }
+
+            if(count === 5){
+                winner = turn;
+            }
+        }
+    }
+}
+
+function checkDiag(){
+    for(let i=4; i<size; i++){
+        for(let j=0; j<size-4; j++){
+            const target = `y${i}x${j}`;
+            const box = map.querySelector(`#${target}`);
+            const text = box.innerText;
+            let count = 0;
+            if(text === (turn == 1 ? "⚫️" : "⚪️")){
+                for(let k=0; k<5; k++){
+                const target2 = `y${i-k}x${j+k}`;
+                const box = map.querySelector(`#${target2}`);
+                const text2 = box.innerText;
+                if(text2 === (turn == 1 ? "⚫️" : "⚪️")){
+                    count ++;
+                    }
+                }
+            }
+            if(count === 5){
+                win = turn;
+            }
+        }
+    }
+}
+
+function checkDiag2(){
+    for(let i=0; i<size-4; i++){
+        for(let j=0; j<size-4; j++){
+        const target = `y${i}x${j}`;
+        const box = map.querySelector(`#${target}`);
+        const text = box.innerText;
+        let count = 0;
+        if(text === (turn == 1 ? "⚫️" : "⚪️")){
+            for(let k=0; k<5; k++){
+                const target2 = `y${i+k}x${j+k}`;
+                const box = map.querySelector(`#${target2}`);
+                const text2 = box.innerText;
+                if(text2 === (turn == 1 ? "⚫️" : "⚪️")){
+                    count ++;
+                }
+            }
+        }
+        if(count === 5){
+            win = turn;
+        }
+        }
+    }
+}
+
+function reset(){
+    turn = 1;
+    win = 0;
+    location.reload();
 }
